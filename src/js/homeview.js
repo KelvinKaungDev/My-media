4a8860a9f2f60b5cedf79c79142908dd36add8f7
Kaung Htet Oo <kaunghtetoo.dev@gmail.com>
2022-07-22 13:45:08 +0700

    get server client

--- src/js/homeview.js
+++ src/js/homeview.js
@@ -3,13 +3,22 @@ export default {
     name: 'HomeView',
     data () {
         return {
-            posts: []
+            posts : [],
+            getPostTitle : ""
         }
     },
     methods: {
         getPosts() {
             axios.get('http://localhost:8000/api/posts').then((response) => {
                 this.posts = response.data.posts;
+            })
+        },
+        searchPost() {
+            let postKey = {
+                key : this.getPostTitle
+            }
+            axios.post('http://localhost:8000/api/post/search', postKey).then((response) => {
+                console.log(response.data)
             })
         }
     },
